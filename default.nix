73124b042b0ab4fb9560dd0e38f471a7b8f8ca97
Peter Jones <pjones@devalot.com>
2020-06-08 14:06:27 -0700

    Upgrade nix-hs

--- default.nix
+++ default.nix
@@ -1,24 +1,13 @@
-{ sources ? import ./nix/sources.nix
-, pkgs ? import sources.nixpkgs { }
-, nix-hs ? import sources.nix-hs { inherit pkgs; }
-, ghc ? "default"
-, ghcide ? sources.ghcide-nix
-, ormolu ? sources.ormolu
-}:
+{ sources ? import ./nix/sources.nix, pkgs ? import sources.nixpkgs { }
+, nix-hs ? import sources.nix-hs { inherit pkgs; }, ghc ? "default" }:
 
 nix-hs {
   cabal = ./openid-connect.cabal;
-  flags = ["example"];
+  flags = [ "example" ];
   compiler = ghc;
-  overrides = lib: self: super: with lib; {
-    # Version 0.8 has an overly restrictive constraint on base:
-    http-media = doJailbreak super.http-media;
-
-    ghcide = import ghcide {};
-
-    ormolu = (import ormolu {
-      inherit (lib) pkgs;
-      ormoluCompiler = lib.compilerName;
-    }).ormolu;
-  };
+  overrides = lib: self: super:
+    with lib; {
+      # Version 0.8 has an overly restrictive constraint on base:
+      http-media = doJailbreak super.http-media;
+    };
 }
